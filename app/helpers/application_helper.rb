67745d8dd0b58eb868d868075951f7afd1decf86
kai he <witty98@126.com>
2016-01-16 18:24:41 +0800

    Finish layout and routs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "项目管理登记系统"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
